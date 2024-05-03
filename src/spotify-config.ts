b6f510c557372f65dc9dc500e3aec76949aef762
Dillon Mulroy <dillon.mulroy@gmail.com>
2024-05-03 13:55:02 -0400

    updates

---
+++ src/spotify-config.ts
@@ -0,0 +1,42 @@
+import { Config, Context, Effect, Layer, Secret } from "effect";
+import AccessTokenJson from "./do_not_open_on_stream/access-token.json";
+import type { AccessToken } from "@spotify/web-api-ts-sdk";
+
+// TODO Schema decode
+const accessToken: AccessToken = AccessTokenJson as unknown as AccessToken;
+
+export type ISpotifyConfig = Readonly<{
+  accessToken: AccessToken;
+  clientId: string;
+  clientSecret: Secret.Secret;
+  port: number;
+  redirectServerPath: string;
+}>;
+
+function make() {
+  return Effect.gen(function* (_) {
+    const clientId = yield* Config.string("SPOTIFY_CLIENT_ID");
+    const clientSecret = yield* Config.secret("SPOTIFY_CLIENT_SECRET");
+    const port = yield* Config.number("REDIRECT_SERVER_PORT").pipe(
+      Config.withDefault(3939),
+    );
+    const redirectServerPath = yield* Config.string(
+      "REDIRECT_SERVER_PATH",
+    ).pipe(Config.withDefault("redirect"));
+
+    return {
+      accessToken,
+      clientId,
+      clientSecret,
+      port,
+      redirectServerPath,
+    } as const;
+  });
+}
+
+export class SpotifyConfig extends Context.Tag("spotify-config")<
+  SpotifyConfig,
+  ISpotifyConfig
+>() {
+  static Live = Layer.effect(SpotifyConfig, make());
+}
--- src/twitch-service.ts
+++ src/twitch-service.ts
@@ -1,19 +1,10 @@
-import {
-  Context,
-  Layer,
-  Effect,
-  Secret,
-  Queue,
-  pipe,
-  Data,
-  PubSub,
-} from "effect";
+import { Context, Layer, Effect, Secret, Queue, Data } from "effect";
 import {
   RefreshingAuthProvider,
   StaticAuthProvider,
   type AuthProvider,
 } from "@twurple/auth";
-import { TwitchConfig, type ITwitchConfig } from "./twitch-config-service";
+import { TwitchConfig } from "./twitch-config";
 import { ApiClient } from "@twurple/api";
 import { EventSubWsListener } from "@twurple/eventsub-ws";
 import { Message, MessagePubSub } from "./message-pubsub";
@@ -24,7 +15,9 @@ export class TwitchAuthProvider extends Context.Tag("twitch-auth-provider")<
 >() {
   static RefreshingAuthProviderLive = Layer.effect(
     this,
-    Effect.map(TwitchConfig, (config) => {
+    Effect.gen(function* () {
+      const config = yield* TwitchConfig;
+
       const authProvider = new RefreshingAuthProvider({
         clientId: config.clientId,
         clientSecret: Secret.value(config.clientSecret),
@@ -32,20 +25,22 @@ export class TwitchAuthProvider extends Context.Tag("twitch-auth-provider")<
       });
 
       return authProvider;
-    })
+    }),
   ).pipe(Layer.provide(TwitchConfig.Live));
 
   static StaticAuthProviderLive = Layer.effect(
     this,
-    Effect.map(TwitchConfig, (config) => {
+    Effect.gen(function* () {
+      const config = yield* TwitchConfig;
+
       const authProvider = new StaticAuthProvider(
         config.clientId,
         Secret.value(config.accessToken),
-        config.scopes
+        config.scopes,
       );
 
       return authProvider;
-    })
+    }),
   ).pipe(Layer.provide(TwitchConfig.Live));
 }
 
@@ -53,23 +48,30 @@ export class TwitchError extends Data.TaggedError("TwitchError")<{
   cause: unknown;
 }> {}
 
+export type ITwitchApiClient = Readonly<{
+  client: ApiClient;
+  useApi: <A>(
+    fn: (client: ApiClient) => Promise<A>,
+  ) => Effect.Effect<A, TwitchError, never>;
+}>;
+
 const makeApiClient = Effect.gen(function* () {
   const authProvider = yield* TwitchAuthProvider;
   const client = new ApiClient({ authProvider });
-  const use = <A>(f: (client: ApiClient) => Promise<A>) =>
+  const useApi = <A>(f: (client: ApiClient) => Promise<A>) =>
     Effect.tryPromise({
       try: () => f(client),
       catch: (error) => new TwitchError({ cause: error }),
     });
-  return { use, client } as const;
+  return { useApi, client } as const;
 });
 
-export class Twitch extends Context.Tag("app/Twitch")<
-  Twitch,
-  Effect.Effect.Success<typeof makeApiClient>
+export class TwitchApiClient extends Context.Tag("app/Twitch")<
+  TwitchApiClient,
+  ITwitchApiClient
 >() {
   static Live = Layer.effect(this, makeApiClient).pipe(
-    Layer.provide(TwitchAuthProvider.RefreshingAuthProviderLive)
+    Layer.provide(TwitchAuthProvider.RefreshingAuthProviderLive),
   );
 }
 
@@ -80,7 +82,7 @@ export class TwitchEventSubClient extends Context.Tag("twitch-eventsub-client")<
   static Live = Layer.scoped(
     this,
     Effect.gen(function* () {
-      const api = yield* Twitch;
+      const api = yield* TwitchApiClient;
 
       const eventsub = yield* Effect.acquireRelease(
         Effect.sync(() => new EventSubWsListener({ apiClient: api.client })),
@@ -89,18 +91,18 @@ export class TwitchEventSubClient extends Context.Tag("twitch-eventsub-client")<
             yield* Effect.logInfo("event sub stopping");
             eventsub.stop();
             return Effect.void;
-          })
+          }),
       );
       yield* Effect.logInfo("event sub starting");
 
       return eventsub;
-    })
-  ).pipe(Layer.provide(Twitch.Live));
+    }),
+  ).pipe(Layer.provide(TwitchApiClient.Live));
 
   static Test = Layer.scoped(
     this,
     Effect.gen(function* () {
-      const api = yield* Twitch;
+      const api = yield* TwitchApiClient;
 
       const eventsub = yield* Effect.acquireRelease(
         Effect.sync(
@@ -108,39 +110,39 @@ export class TwitchEventSubClient extends Context.Tag("twitch-eventsub-client")<
             new EventSubWsListener({
               apiClient: api.client,
               url: "ws://127.0.0.1:8080/ws",
-            })
+            }),
         ),
         (eventsub) => {
           eventsub.stop();
 
           return Effect.void.pipe(() =>
-            Effect.logInfo("eventsub disconnected")
+            Effect.logInfo("eventsub disconnected"),
           );
-        }
+        },
       );
       yield* Effect.logInfo("connected");
 
       return eventsub;
-    })
-  ).pipe(Layer.provide(Twitch.Live));
+    }),
+  ).pipe(Layer.provide(TwitchApiClient.Live));
 }
 
-const TwitchClientsLive = Layer.mergeAll(
+export const TwitchClientsLive = Layer.mergeAll(
   TwitchConfig.Live,
-  Twitch.Live,
-  TwitchEventSubClient.Live
+  TwitchApiClient.Live,
+  TwitchEventSubClient.Live,
 );
 
-const TwitchClientsTest = Layer.mergeAll(
+export const TwitchClientsTest = Layer.mergeAll(
   TwitchConfig.Live,
-  Twitch.Live,
-  TwitchEventSubClient.Test
+  TwitchApiClient.Live,
+  TwitchEventSubClient.Test,
 );
 
 export const TwitchService = Layer.scopedDiscard(
   Effect.gen(function* () {
     yield* Effect.logInfo("twitch service starting");
-    const api = yield* Twitch;
+    const api = yield* TwitchApiClient;
     const config = yield* TwitchConfig;
     const eventsub = yield* TwitchEventSubClient;
     const pubsub = yield* MessagePubSub;
@@ -159,10 +161,10 @@ export const TwitchService = Layer.scopedDiscard(
 
         if (event.messageText === "!song") {
           await Effect.runPromise(
-            pubsub.publish(Message.CurrentlyPlayingRequest())
+            pubsub.publish(Message.CurrentlyPlayingRequest()),
           );
         }
-      }
+      },
     );
 
     // eventsub.onChannelRedemptionAddForReward(
@@ -175,16 +177,23 @@ export const TwitchService = Layer.scopedDiscard(
     //   },
     // );
 
-    yield* Effect.gen(function* () {
-      yield* Effect.logInfo("waiting for song");
-      const { song } = yield* Queue.take(dequeue);
-      yield* Effect.logInfo("received song");
-      yield* api.use((_) =>
-        _.chat.sendChatMessage(
-          config.broadcasterId,
-          `The currently playing song is ${song.name}`
-        )
-      );
-    }).pipe(Effect.forever, Effect.forkScoped);
-  })
+    yield* Effect.forkScoped(
+      Effect.forever(
+        Effect.gen(function* () {
+          yield* Effect.logInfo("waiting for song");
+
+          const { song } = yield* Queue.take(dequeue);
+
+          yield* Effect.logInfo("received song");
+
+          yield* api.useApi((client) =>
+            client.chat.sendChatMessage(
+              config.broadcasterId,
+              `The currently playing song is ${song.name}`,
+            ),
+          );
+        }),
+      ),
+    );
+  }),
 ).pipe(Layer.provide(Layer.mergeAll(TwitchClientsLive)));
