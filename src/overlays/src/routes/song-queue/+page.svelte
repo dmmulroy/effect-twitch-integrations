175ae2340910c83efaeee9b029c661744a499a82
Dillon Mulroy <dillon.mulroy@gmail.com>
2024-05-26 13:15:31 -0400

    update song queue overlay

--- src/overlays/src/routes/song-queue/+page.svelte
+++ src/overlays/src/routes/song-queue/+page.svelte
@@ -2,41 +2,60 @@
   import { Option } from "effect";
   import { onMount } from "svelte";
   import { invalidate } from "$app/navigation";
-
   export let data;
+
+  let active: "currentlyPlaying" | "nextUp" = "currentlyPlaying";
+  let activeData: typeof data.currentlyPlaying;
+
+  $: activeData =
+    active === "currentlyPlaying" ? data.currentlyPlaying : data.nextUp;
 
   onMount(() => {
     const interval = setInterval(() => {
+      if (active === "currentlyPlaying") {
+        active = "nextUp";
+        return;
+      }
+
+      active = "currentlyPlaying";
       invalidate("song-queue");
     }, 5000);
 
-    return () => clearInterval(interval);
+    return () => {
+      clearInterval(interval);
+    };
   });
 </script>
 
 <div
   class="mx-auto mt-5 flex w-80 h-[120px] items-center rounded-md bg-[#24273a] p-2 text-[#cad3f5] shadow-lg"
 >
-  <img
-    src={data?.currentlyPlaying?.track.album.images[0]?.url}
-    width="64"
-    height="64"
-    alt="Album Art"
-    class="rounded-full animate-spin-slow"
-  />
+  {#if activeData?.track.album.images[0]?.url !== undefined}
+    <img
+      src={activeData?.track.album.images[0]?.url}
+      width="64"
+      height="64"
+      alt="Album Art"
+      class="rounded-full animate-spin-slow"
+    />
+  {/if}
   <div class="flex-grow">
     <div class="pl-4 mb-2 flex items-center">
       <div class="flex flex-col gap-1">
-        <h2 class="text-sm font-semibold">Now Playing</h2>
-        <p class="text-xs">{data.currentlyPlaying?.track.name}</p>
+        {#if activeData !== undefined}
+          <h2 class="text-sm font-semibold">
+            {active === "currentlyPlaying" ? "Now Playing" : "Next Up"}
+          </h2>
+        {:else}
+          <h2 class="text-sm font-semibold">Nothing is currently playing</h2>
+        {/if}
+        <p class="text-xs">{activeData?.track.name}</p>
         <p class="text-xs text-[#b8c0e0]">
-          {data.currentlyPlaying?.track.artists
-            .map(({ name }) => name)
-            .join(", ")}
+          {activeData?.track.artists.map(({ name }) => name).join(", ")}
         </p>
-        {#if data.currentlyPlaying && Option.isSome(data.currentlyPlaying.requesterDisplayName)}
+        {#if activeData && Option.isSome(activeData.requesterDisplayName)}
           <p class="text-xs text-[#b8c0e0]">
-            Requested by @{data.currentlyPlaying.requesterDisplayName.value}
+            Requested by @{activeData.requesterDisplayName.value}
           </p>
         {/if}
       </div>
@@ -50,7 +69,7 @@
     stroke-width="2"
     stroke-linecap="round"
     stroke-linejoin="round"
-    class="text-[#f5bde6] h-12 w-12 ml-auto"
+    class="text-[#f5bde6] min-h-12 min-w-12 ml-auto"
   >
     <path d="M9 18V5l12-2v13"></path>
     <circle cx="6" cy="18" r="3"></circle>
--- src/overlays/svelte.config.js
+++ src/overlays/svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from "@sveltejs/adapter-vercel";
+import vercel_adapter from "@sveltejs/adapter-vercel";
+import bun_adapter from "svelte-adapter-bun";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,7 +12,8 @@ const config = {
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter(),
+    adapter:
+      process.env.NODE_ENV === "production" ? vercel_adapter() : bun_adapter(),
   },
 };
 
